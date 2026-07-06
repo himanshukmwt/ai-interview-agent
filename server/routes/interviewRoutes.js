de6867e7674dcd439327f9e9521f8083376fdabc
himanshukmwt <himanshukumawat8824@gmail.com>
2026-07-06 22:31:32 +0530

    Routes added for interview functions

--- server/routes/interviewRoutes.js
+++ server/routes/interviewRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import isAuth from '../middleware/isAuth.js';
 import {upload} from "../middleware/multer.js"
-import { analyzeresume,submitAnswer,finishInterview, generateQuestion } from '../controllers/interviewController.js';
+import { analyzeresume,submitAnswer,finishInterview, generateQuestion, getMyInterviews, getInterviewReport } from '../controllers/interviewController.js';
 
 const router=express.Router();
 
@@ -10,5 +10,7 @@ router.post("/resume",isAuth,upload.single("resume"),analyzeresume);
 
 router.post("/submit-answer",isAuth,submitAnswer);
 router.post("/finish",isAuth,finishInterview);
+router.get("/get-interview",isAuth,getMyInterviews);
+router.get("/report/:id",isAuth,getInterviewReport);
 
 export default router;
