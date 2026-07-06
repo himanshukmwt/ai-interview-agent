a59056ee6c7ea9d5cbe8d3ac0d76b47e537bf465
himanshukmwt <himanshukumawat8824@gmail.com>
2026-07-06 13:05:32 +0530

    added voice in interview

--- client/src/components/Interview.jsx
+++ client/src/components/Interview.jsx
@@ -3,9 +3,145 @@ import femaleVideo from "../assets/videos/female-ai.mp4";
 import { motion } from "motion/react";
 import Timer from "./Timer";
 import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
 
 function Interview({ interviewData, onFinish }) {
   const {interviewId, questions,userName}=interviewData;
+  const [isIntroPhase, setIsIntroPhase] = useState(true);
+
+  const [isMicOn, setIsMicOn] = useState(true);
+  const recognitionRef = useRef(null);
+  const [isAIPlaying, setIsAIPlaying] = useState(false);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [answer, setAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [timeLeft, setTimeLeft] = useState(
+    questions[0]?.timeLimit || 60
+  );
+
+  const [selectedVoice, setSelectedVoice] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [voiceGender, setVoiceGender] = useState("female");
+  const [subtitle, setSubtitle] = useState("");
+
+  const videoRef=useRef(null);
+  const currentQuestion=questions[currentIndex];
+
+
+  useEffect(()=>{
+    const loadVoices=()=>{
+      const voices=window.speechSynthesis.getVoices();
+      if(!voices.length)return;
+
+      //known female voices
+      const femaleVoice =
+        voices.find(v =>
+          v.name.toLowerCase().includes("zira")||
+          v.name.toLowerCase().includes("samantha")||
+          v.name.toLowerCase().includes("female")
+        );
+
+        if(femaleVoice){
+          setSelectedVoice(femaleVoice);
+          setVoiceGender("female");
+          return;
+        }
+
+        //known male voices
+        const maleVoice=
+          voices.find(v =>
+          v.name.toLowerCase().includes("david")||
+          v.name.toLowerCase().includes("mark")||
+          v.name.toLowerCase().includes("male")
+        );
+
+        if(maleVoice){
+          setSelectedVoice(maleVoice);
+          setVoiceGender("male");
+          return;
+        }
+
+        setSelectedVoice(voices[0]);
+        setVoiceGender("female");
+    };
+
+    loadVoices();
+    window.speechSynthesis.onvoiceschanged=loadVoices;
+  },[]);
+
+  const videoSource=voiceGender==="male"?maleVideo:femaleVideo;
+
+  //speak function
+  const speakText=(text)=>{
+    return new Promise((resolve)=>{
+      if(!window.speechSynthesis || !selectedVoice){
+        resolve();
+        return;
+      }
+
+      window.speechSynthesis.cancel();
+
+      const humanText= text
+          .replace(/,/g, ", ...")
+          .replace(/\./g, ". ...");
+
+      const utterance =new SpeechSynthesisUtterance(humanText);
+
+      utterance.voice = selectedVoice;
+
+      utterance.rate=0.92;
+      utterance.pitch=1.05;
+      utterance.volume=1;
+
+      utterance.onstart = () => {
+        setIsAIPlaying(true);
+        videoRef.current?.play();
+      };
+
+      utterance.onend = () => {
+        videoRef.current?.pause();
+        videoRef.current.currntTime =0;
+        setIsAIPlaying(false);
+      
+
+      setTimeout(()=>{
+        setSubtitle("");
+        resolve();
+      },300);
+    };
+
+    setSubtitle(text);
+
+    window.speechSynthesis.speak(utterance);
+
+    })
+  };
+
+  useEffect(()=>{
+    if(!selectedVoice){
+      return;
+    }
+    const runIntro=async()=>{
+      if(isIntroPhase){
+      await speakText(
+        `Hi ${userName}, it's great to meet you today. I'll ask you a few questions, Just answer naturally, and take your time. let's begin`
+      );
+      setIsIntroPhase(false);
+
+    }
+    else if(currentQuestion){
+      await new Promise(r => setTimeout(r,800));
+
+      if(currentIndex === questions.length -1){
+        await speakText("Alright, this one might be a bit more challenging");
+      }
+      await speakText(currentQuestion.question);
+    }
+  }
+  runIntro();
+  },[selectedVoice , isIntroPhase , currentIndex])
+
 
   return (
     <div className="min-h-screen bg-linear-to-br from-indigo-300 via-white to-purple-300 flex items-center justify-center p-4 sm:p-6">
@@ -13,16 +149,34 @@ function Interview({ interviewData, onFinish }) {
         {/* video section */}
         <div className="w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200">
           <div className="w-full max-w-md rounded-2xl overflow-hidden shadow-xl">
-            <video src={femaleVideo} className="w-full h-auto object-cover" />
+            <video src={videoSource} 
+                    key={videoSource}
+                    ref={videoRef}
+                    muted
+                    playsInline
+                    preload="auto"
+            className="w-full h-auto object-cover" />
           </div>
+
+          {/* subtitle */}
+          {subtitle && (
+            <div className="w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm">
+              <p className="text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed">
+                {subtitle}
+              </p>
+
+            </div>
+          )}
+
 
           {/* timer */}
           <div className="w-full max-w-md bg-white border border-gary-200 rounded-2xl shadow-md p-6 space-y-5">
             <div className="flex justify-between items-center">
               <span className="text-sm text-gray-500">Interview Status</span>
-              <span className="text-sm font-semibold text-indigo-400">
-                AI Speaking
-              </span>
+              {isAIPlaying && (
+                <span className="text-sm font-semibold text-indigo-400">
+                {isAIPlaying ? "AI Speaking": ""}
+              </span>)}
             </div>
             <div className="h-px bg-gray-200"></div>
 
@@ -34,11 +188,11 @@ function Interview({ interviewData, onFinish }) {
 
             <div className="grid grid-cols-2 gap-6 text-center">
               <div>
-                <span className="text-2xl font-bold text-blue-600">1</span>
+                <span className="text-2xl font-bold text-blue-600">{currentIndex+1}</span>
                 <span className="text-xs text-gray-400">Current Questions</span>
               </div>
               <div>
-                <span className="text-2xl font-bold text-blue-600">5</span>
+                <span className="text-2xl font-bold text-blue-600">{questions.length}</span>
                 <span className="text-xs text-gray-400">Total Questions</span>
               </div>
             </div>
@@ -53,10 +207,10 @@ function Interview({ interviewData, onFinish }) {
 
           <div className="relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm">
             <p className="text-xs sm:text-sm text-gray-400 mb-2">
-              Question 1 of 5
+              Question {currentIndex+1} of {questions.length}
             </p>
             <div className="text-base sm:text-lg font-semibold text-gray-800  leading-relaxed">
-              First Question
+              {currentQuestion?.question}
             </div>
           </div>
           <textarea
