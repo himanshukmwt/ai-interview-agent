ffef521b17d3cc824e6c0bb5d40ce78e471177f7
himanshukmwt <himanshukumawat8824@gmail.com>
2026-07-19 16:39:49 +0530

    app.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { currentUser } from "./services/api";
