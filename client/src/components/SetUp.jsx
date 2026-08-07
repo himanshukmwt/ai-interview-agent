c6e35564df02abd839813b0d3b0044437f883865
himanshukmwt <himanshukumawat8824@gmail.com>
2026-08-07 22:58:05 +0530

    interview setup Ui updated

--- client/src/components/SetUp.jsx
+++ client/src/components/SetUp.jsx
@@ -8,12 +8,14 @@ import {
   FaChartLine,
 } from "react-icons/fa";
 import { resumeUPLOAD, startInterview } from "../services/api";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUserData } from "../redux/userSlice";
+import robot from "../assets/robot.png";
+import { HiSparkles } from "react-icons/hi";
 
 function SetUp({ onStart }) {
-  const userData=useSelector((state)=>state.action);
-  const dispatch=useDispatch();
+  const userData = useSelector((state) => state.action);
+  const dispatch = useDispatch();
   const [role, setRole] = useState("");
   const [experience, setExperience] = useState("Fresher");
   const [mode, setMode] = useState("Technical");
@@ -50,58 +52,72 @@ function SetUp({ onStart }) {
     }
   };
 
-  const handleStart=async()=>{
+  const handleStart = async () => {
     setLoading(true);
-    try{
-      const result=await startInterview({role,experience,mode,resumeText,projects,skills});
+    try {
+      const result = await startInterview({
+        role,
+        experience,
+        mode,
+        resumeText,
+        projects,
+        skills,
+      });
       console.log(result.data);
-      if(userData){
-        dispatch(setUserData({...userData, credits:result.data.creditLeft}));
+      if (userData) {
+        dispatch(setUserData({ ...userData, credits: result.data.creditLeft }));
       }
       setLoading(false);
       onStart(result.data);
-    }catch(error){
+    } catch (error) {
       console.log(error);
-    }finally{
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
-      className="min-h-screen flex items-center justify-center bg-linear-to-br from-gray-100 to-gray-200 px-4"
+      className="min-h-screen flex items-center justify-center  bg-[#F7F6FE] px-4"
     >
-      <div className="w-full max-w-5xl bg-white rounded-3xl shadow-2xl grid md:grid-cols-2 overflow-hidden">
+      <div className="w-full max-w-5xl grid md:grid-cols-2 gap-14 items-center">
         <motion.div
           initial={{ x: -80, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.6 }}
-          className="relative bg-linear-to-br from-blue-50 to-blue-100 p-12 flex flex-col justify-center"
+          className="max-w-xl space-y-1"
         >
-          <h2 className="text-4xl font-bold text-gray-800 mb-6">
-            Start Your AI Interview
-          </h2>
-          <p className="text-gray-600 mb-10">
-            Master real-world interview scenarios with AI. Sharpen your
-            communication, technical expertise, and confidence to ace every
-            interview.
+          <div className="inline-flex items-center px-4 py-2 rounded-full bg-indigo-100 text-indigo-700 font-medium text-sm mt-2">
+            <HiSparkles size={16} className="bg-indigo-100 text-yellow-400 " />
+             AI Powered Interview Platform
+          </div>
+          <h1 className="text-4xl font-bold leading-tight text-gray-900">
+            Ace Your Next
+            <span className="text-indigo-600">{" "} AI Interview</span>
+          </h1>
+          <p className="text-md text-gray-500 leading-8 max-w-lg">
+            Practice realistic interviews powered by AI. Receive smart follow-up
+            questions, instant feedback, and detailed performance analytics.
           </p>
-          <div className="space-y-5">
+
+          <div className="grid grid-cols-1 md:grid-cols-[1fr_1.3fr] gap-6 items-center">
+          
+          <div className="space-y-2">
             {[
               {
-                icon: <FaUserTie className="text-purple-800 text-xl" />,
-                text: "Choose Role & Experience",
+                icon: <FaUserTie className="text-indigo-600 text-xl" />,
+                text: "Resume-based Questions",
               },
               {
-                icon: <FaMicrophoneAlt className="text-purple-800 text-xl" />,
-                text: "Smart Voice Interview",
+                icon: <FaMicrophoneAlt className="text-indigo-600 text-xl" />,
+                text: "AI Follow-up Questions",
               },
               {
-                icon: <FaChartLine className="text-purple-800 text-xl" />,
-                text: "Performance Analytics",
+                icon: <FaChartLine className="text-indigo-600 text-xl" />,
+                text: "Performance Dashboard",
               },
             ].map((item, index) => (
               <motion.div
@@ -110,12 +126,29 @@ function SetUp({ onStart }) {
                 transition={{ duration: 0.4 + index * 0.2 }}
                 whileHover={{ scale: 1.03 }}
                 key={index}
-                className="flex items-center space-x-4 bg-white p-4 rounded-xl shadow-sm cursor-pointer"
+                className="flex items-center gap-4
+                  rounded-xl
+                  border
+                  border-gray-200
+                  bg-white
+                  px-5
+                  py-4
+                  hover:border-indigo-500
+                  hover:shadow-lg
+                  transition"
               >
                 {item.icon}
                 <span className="text-gray-700 font-medium">{item.text}</span>
               </motion.div>
             ))}
+          </div>
+          <div className="mb-1">
+            <img
+                src={robot}
+                alt="AI Interview"
+                className="w-72 mx-auto "
+            />
+        </div>
           </div>
         </motion.div>
 
@@ -123,10 +156,11 @@ function SetUp({ onStart }) {
           initial={{ x: 80, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8 }}
-          className="px-8 py-4 bg-amber-50"
+          className="
+            bg-white rounded-3xl shadow-xl border border-gray-100 px-8 py-5"
         >
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">
-            Interview SetUp
+          <h2 className="text-2xl font-bold text-center text-gray-800 mb-4">
+            Interview Setup
           </h2>
           <div className="space-y-3">
             <div className="relative">
@@ -134,7 +168,8 @@ function SetUp({ onStart }) {
               <input
                 type="text"
                 placeholder="Software Engineer"
-                className="w-full pl-12 pr-4 py-3 border border-gray-300 rounded-xl focus:ring-1 focus:ring-blue-300 outline-none transition"
+                className="w-full pl-12 pr-4 py-4 border border-gray-200 rounded-xl focus:ring-2
+                  focus:ring-indigo-500 focus:border-indigo-500 outline-none transition"
                 onChange={(e) => setRole(e.target.value)}
                 value={role}
               />
@@ -142,30 +177,25 @@ function SetUp({ onStart }) {
 
             <div className="relative">
               <FaBriefcase className="absolute top-4 left-4 text-gray-400" />
-              {/* <input
-                type="text"
-                placeholder="Experience (e.g 1-3 Years)"
-                className="w-full pl-12 pr-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-300 outline-none transition"
-                onChange={(e) => setExperience(e.target.value)}
-                value={experience}
-              /> */}
               <select
-              value={experience}
-              onChange={(e) => setExperience(e.target.value)}
-              className="w-full py-3 pl-12 pr-4 border border-gray-200 rounded-xl focus:ring-1 focus:ring-blue-300 outline-none transition text-gray-600 cursor-pointer"
-            >
-              <option value="Fresher">Fresher</option>
-              <option value="1-2 years">1-2 years</option>
-               <option value="3-5 years">3-5 years</option>
+                value={experience}
+                onChange={(e) => setExperience(e.target.value)}
+                className="w-full py-4 pl-12 pr-4 border border-gray-200 rounded-xl focus:ring-2
+                    focus:ring-indigo-500 focus:border-indigo-500 outline-none transition text-gray-600 cursor-pointer"
+              >
+                <option value="Fresher">Fresher</option>
+                <option value="1-2 years">1-2 years</option>
+                <option value="3-5 years">3-5 years</option>
                 <option value="5-8 years">5-8 years</option>
-                 <option value="8+ years">8+ years</option>
-            </select>
+                <option value="8+ years">8+ years</option>
+              </select>
             </div>
 
             <select
               value={mode}
               onChange={(e) => setMode(e.target.value)}
-              className="w-full py-3 px-4 border border-gray-200 rounded-xl focus:ring-1 focus:ring-blue-300 outline-none transition text-gray-600 cursor-pointer"
+              className="w-full py-4 px-4 border border-gray-200 rounded-xl focus:ring-2
+              focus:ring-indigo-500 focus:border-indigo-500 outline-none transition text-gray-600 cursor-pointer"
             >
               <option value="Technical">Technical Interview</option>
               <option value="HR">HR Interview</option>
@@ -174,9 +204,9 @@ function SetUp({ onStart }) {
             {!analysisDone && (
               <motion.div
                 onClick={() => document.getElementById("resumeUpload").click()}
-                className="border-2 border-dashed border-gray-300 rounded-xl p-5 text-center cursor-pointer hover:border-blue-400 transition"
+                className="border-2 border-dashed border-gray-300 rounded-xl p-5 text-center cursor-pointer hover:border-indigo-500 transition"
               >
-                <FaFileUpload className="text-4xl mx-auto text-blue-300 mb-3" />
+                <FaFileUpload className="text-4xl mx-auto text-indigo-400 mb-3" />
                 <input
                   type="file"
                   accept="application/pdf"
@@ -184,10 +214,10 @@ function SetUp({ onStart }) {
                   className="hidden"
                   onChange={(e) => setResumeFile(e.target.files[0])}
                 />
-                <p className="text-gray-500 font-medium">
+                <p className="text-gray-500 font-medium ">
                   {resumeFile
                     ? resumeFile.name
-                    : "Click to upload resume(Optional)"}
+                    : "Drag & Drop Resume or Browse Files (Optional)"}
                 </p>
 
                 {resumeFile && (
@@ -197,7 +227,7 @@ function SetUp({ onStart }) {
                       e.stopPropagation();
                       handleUploadResume();
                     }}
-                    className="mt-4 bg-gray-500 text-white px-5 py-2 rounded-xl hover:bg-gray-600 transition cursor-pointer"
+                    className="mt-4 bg-indigo-600 hover:bg-indigo-700 text-white px-5 py-2 rounded-xl transition cursor-pointer"
                   >
                     {analyzing ? "Analyzing..." : "Analyze Resume"}
                   </motion.button>
@@ -230,7 +260,7 @@ function SetUp({ onStart }) {
                       {skills.map((s, i) => (
                         <li
                           key={i}
-                          className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-sm"
+                          className="bg-indigo-100 text-indigo-600 px-3 py-1 rounded-full text-sm"
                         >
                           {s}
                         </li>
@@ -241,13 +271,14 @@ function SetUp({ onStart }) {
               </motion.div>
             )}
             <motion.button
-            onClick={handleStart}
+              onClick={handleStart}
               disabled={!role || !experience || loading}
               whileHover={{ scale: 1.03 }}
               whileTap={{ scale: 0.95 }}
-              className="w-full disabled:bg-gray-400 bg-blue-400 hover:bg-blue-500 text-white py-2.5 rounded-full text-lg font-semibold transition duration-300 shadow-md cursor-pointer"
+              className="w-full disabled:bg-gray-400 bg-indigo-600
+              hover:bg-indigo-700 text-white py-3 rounded-xl text-lg font-semibold transition duration-300 shadow-md cursor-pointer"
             >
-              {loading ? "Starting...":"Start Interview"}
+              {loading ? "Starting..." : "Start Interview"}
             </motion.button>
           </div>
         </motion.div>
