d75a03d53efcfa7fe7b13a517bee48f17764613d
himanshukmwt <himanshukumawat8824@gmail.com>
2026-08-10 22:20:36 +0530

    index.js updated

--- server/index.js
+++ server/index.js
@@ -20,8 +20,10 @@ app.use(helmet());
 const PORT=process.env.PORT || 8100;
 app.use(
   cors({
-    // origin: process.env.CLIENT_URL,
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
+    // origin: [
+    //   "http://localhost:5173",
+    // process.env.CLIENT_URL],
     credentials: true,
   })
 );
